5747fcbc9f715e94c0354611c61577f6e93667ff
Kweebac <06.amarshall@gmail.com>
2023-03-05 17:07:28 +0000

    Changed the createGrid function so it doesn't crash, and boxes now have an outline

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 function changeGridSize() {
-  const num = (+prompt("What size do you want the canvas to be? 0-100")) ** 2;
+  const num = +prompt("What size do you want the canvas to be? 0-100");
   if (num <= 0 || num > 100) {
     alert("Invalid");
     return;
   }
 
   removeGrid();
-
-  for (let i = 0; i < num; i++) {
-    const NEW_DIV = document.createElement("div");
-    CONTAINER_DIV.appendChild(NEW_DIV);
-  }
+  createGrid(num);
 }
 
 function removeGrid() {
   const count = CONTAINER_DIV.childElementCount;
   for (let i = 0; i < count; i++) {
     CONTAINER_DIV.removeChild(document.querySelector(".container div"));
+  }
+}
+
+function createGrid(num) {
+  for (let i = 0; i < num; i++) {
+    const NEW_DIV = document.createElement("div");
+    CONTAINER_DIV.appendChild(NEW_DIV);
+
+    for (let j = 0; j < num; j++) {
+      const NEW_DIV2 = document.createElement("div");
+      NEW_DIV.appendChild(NEW_DIV2);
+    }
   }
 }
 
